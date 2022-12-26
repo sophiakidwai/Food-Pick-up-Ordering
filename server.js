952c8c3b8aeb2b0ebabcfb8794273f2930e689f0
Amina Malik <amina.malik@telus.com>
2022-12-26 00:18:53 -0500

    api route addition

--- server.js
+++ server.js
@@ -30,14 +30,21 @@ app.use(express.static('public'));
 // Note: Feel free to replace the example routes below with your own
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
+const menuApiRoutes = require('./routes/menu-api');
 const usersRoutes = require('./routes/users');
+const menuRoutes = require('./routes/menu');
+const checkoutRoutes = require('./routes/checkout');
+
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
 app.use('/api/users', userApiRoutes);
+app.use('/api/menu', menuApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
+app.use('/menu', menuRoutes);
+app.use('/checkout', checkoutRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
